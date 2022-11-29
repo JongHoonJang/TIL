b4d29632794f133378326c9d8ad24a0344995e00
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2022-11-29 09:27:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 504 ms, Memory: 32304 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,38 @@
+def DFS(n):
+    print(n, end=' ')
+    visited[n] = True
+    for i in sol[n]:
+        if not visited[i]:
+            DFS(i)
+
+
+def BFS(lst, start):
+    visit_bfs = []
+    queue = [start]
+
+    while queue:
+        t = queue.pop(0)
+        visit_bfs.append(t)
+        if not visited[t]:
+            visited[t] = 1
+        for i in lst[t]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = visited[t] + 1
+    else:
+        return visit_bfs
+
+
+n, m, v = map(int, input().split())
+sol = [[] for _ in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    sol[a].append(b)
+    sol[b].append(a)
+for s in sol:
+    s.sort()
+visited = [0] * (n + 1)
+DFS(v)
+visited = [0] * (n + 1)
+print()
+print(*BFS(sol, v))
