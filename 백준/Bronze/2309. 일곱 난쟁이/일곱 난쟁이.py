72724f3f437952362a2ac06c0a6cd3ee07f3b8df
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2022-12-02 17:03:22 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,13 @@
+lst = [int(input()) for _ in range(9)]
+
+for i in range(1 << 9):
+    result = []
+    cnt = 0
+    for j in range(9):
+        if i & (1 << j):
+            result.append(lst[j])
+            cnt += 1
+    if cnt == 7 and sum(result) == 100:
+        for p in sorted(result):
+            print(p)
+        break
