66f57ca77e1de7ea8912e571b45c1ef0e2033f81
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2022-12-02 17:04:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,11 @@
+dice = list(map(int, input().split()))
+
+if dice[0] == dice[1]:
+    if dice[1] == dice[2]:
+        print(10000 + dice[0] * 1000)
+    else:
+        print(1000 + dice[0] * 100)
+elif dice[1] == dice[2] or dice[0] == dice[2]:
+    print(1000 + dice[2] * 100)
+else:
+    print(max(dice) * 100)
