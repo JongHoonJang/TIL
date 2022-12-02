df0152cf1baee436b7d5f7d1018c70168273d202
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2022-12-02 17:06:41 +0900

    [Silver IV] Title: 빙고, Time: 76 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -0,0 +1,38 @@
+def resutlt_bingo(lst_bingo):
+    result = []
+    cresult1 = 0
+    cresult2 = 0
+    for di in range(5):
+        resultx = 0
+        resulty = 0
+        for dj in range(5):
+            resultx += lst_bingo[di][dj]
+            resulty += lst_bingo[dj][di]
+            if di == dj:
+                cresult1 += lst_bingo[di][dj]
+                cresult2 += lst_bingo[di][-(dj + 1)]
+        result.extend([resultx, resulty])
+    result.extend([cresult1, cresult2])
+    return result.count(0)
+
+
+def find(new_bingo, idx):
+    for r in range(5):
+        for c in range(5):
+            if new_bingo[r][c] == x[idx]:
+                new_bingo[r][c] = 0
+    if resutlt_bingo(new_bingo) >= 3:
+        return idx
+    else:
+        return find(new_bingo, idx + 1)
+
+
+
+bingo = [list(map(int, input().split())) for _ in range(5)]
+x = []
+for i in range(5):
+    x += list(map(int, input().split()))
+cnt = 0
+i = 0
+
+print(find(bingo, i)+1)
