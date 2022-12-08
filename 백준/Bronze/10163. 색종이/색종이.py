d00e168a8b3a80266bca6ee3e1101bc488c28e72
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2022-12-08 17:03:04 +0900

    [Bronze I] Title: 색종이, Time: 1804 ms, Memory: 37648 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,12 @@
+paper = [[0] * 1001 for _ in range(1001)]
+N = int(input())
+
+for i in range(1, N + 1):
+    y_p, x_p, a, b = map(int, input().split())
+    for y in range(y_p, y_p + a):
+        paper[y][x_p:x_p + b] = [i] * b
+for k in range(1, N + 1):
+    result = 0
+    for lst in paper:
+        result += lst.count(k)
+    print(result)
