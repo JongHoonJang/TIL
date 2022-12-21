82ad45dcf43f65ebe78be1eb374ffae77e7ff600
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2022-12-21 11:26:43 +0900

    [Bronze I] Title: 딱지놀이, Time: 264 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -0,0 +1,20 @@
+T = int(input())
+for t in range(T):
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    A.pop(0)
+    B.pop(0)
+    result = ''
+    i = 4
+    while i > 0:
+        if A.count(i) > B.count(i):
+            result = 'A'
+            break
+        elif A.count(i) < B.count(i):
+            result = 'B'
+            break
+        else:
+            i -= 1
+    else:
+        result = 'D'
+    print(result)
