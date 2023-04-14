09aef82f7baae25ddc23c46793959d281e6cda82
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-04-14 13:56:21 +0900

    [level 2] Title: 피보나치 수, Time: 161.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,13 +1,8 @@
 def fibonacci(n):
-    if n == 0 or n == 1:
-        return n
-    
-    fib_mem = [0] * (n + 1)
-    fib_mem[0], fib_mem[1] = 0, 1
-    for i in range(n - 1):
-        fib_mem[i + 2] = fib_mem[i] + fib_mem[i + 1]
-    
-    return fib_mem[n]
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
 
 def solution(n):
     return fibonacci(n) % 1234567
