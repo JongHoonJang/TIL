0c06628d898dcfe423cada6b5f74ae908a680897
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-06-19 18:30:52 +0900

    [Bronze III] Title: 신기한 수, Time: 628 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,16 +1,14 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 
 if n < 10:
     print(n)
 else:
     cnt = 9
     for i in range(10, n + 1):
-        res = 0
-        for j in str(i):
-            res += int(j)
-        else:
-            if i % res == 0:
-                cnt += 1
+        res = sum([int(c) for c in str(i)])
+        if i % res == 0:
+            cnt += 1
     print(cnt)
         
 
