3d98fdcb9a436d0d9e4b21533d4330421c0d2295
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-07-17 21:07:30 +0900

    [Bronze III] Title: 11월 11일, Time: 148 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25286. 11월 11일/11월 11일.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())) :
+    y, m = map(int, input().split())
+    check = 0	
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0) :
+        check = 1	
+    
+    if check :
+        days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    if m == 1 :
+        print(y-1, m+11, days[m+10])
+    else :
+        print(y, m-1, days[m-2])
