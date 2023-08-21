4798685130400aed702c4752abe19fbffb79f01f
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-08-21 17:57:18 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,24 @@
+def solution(id_list, report, k):
+    answer = []
+    id_report = {}
+    for re in report:
+        a, b = re.split()
+        if b not in id_report:
+            id_report[b] = [a]
+        else:
+            if a not in id_report[b]:
+                id_report[b].append(a)
+    user = {}
+    for key, value in id_report.items():
+        if len(value) >= k:
+            for j in value:
+                if j not in user:
+                    user[j] = 1
+                else:
+                    user[j] += 1
+    for u in id_list:
+        if u in user:
+            answer.append(user[u])
+        else:
+            answer.append(0)
+    return answer
