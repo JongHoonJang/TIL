cdde35c751eacc4b7bd35568ba128f05224f9331
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-09-07 10:33:26 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.78 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -2,12 +2,6 @@ def solution(board, moves):
     answer = 0
     res = []
     n  = len(board)
-    # [0,0,0,0,0]
-    # [0,0,1,0,3]
-    # [0,2,5,0,1]
-    # [4,2,4,4,2]
-    # [3,5,1,3,1]
-    # [4,3,1,1,3,2,4]
     
     for m in moves:
         for i in range(n):
