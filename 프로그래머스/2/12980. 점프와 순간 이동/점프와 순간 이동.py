a687846402ccb45569ecbbe9a980201f6c0d81f8
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-10-21 12:48:02 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,9 +1,2 @@
 def solution(n):
-    answer = 1
-    while n != 1:
-        if n % 2 == 0:
-            n /= 2
-        else:
-            n -= 1
-            answer += 1
-    return answer
+    return bin(n).count('1')
