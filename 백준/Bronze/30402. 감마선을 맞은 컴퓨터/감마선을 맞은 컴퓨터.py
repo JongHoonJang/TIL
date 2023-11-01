6eb21f967488e2b0c0e17aaf1c22a90092a900d7
장종훈 <97591570+JongHoonJang@users.noreply.github.com>
2023-11-01 22:10:46 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,12 @@
+for i in range(15):
+    arr = list(input().split())
+    if 'w' in arr:
+        print('chunbae')
+        break
+    elif 'b' in arr:
+        print('nabi')
+        break
+    elif 'g' in arr:
+        print('yeongcheol')
+        break
+        
